0e12284f996e6dd119d09cceeab17de6ed6ced39
schminitz <gillian@affinitic.be>
2013-02-13 10:31:29 +0100

    setup for gdwadmin

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ import os
 
 version = '0.1'
 
-setup(name='gites.calendar',
+setup(name='gites.gdwadmin',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.txt").read() + "\n" + open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
       keywords='',
       author='',
       author_email='',
@@ -23,23 +22,10 @@ setup(name='gites.calendar',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'affinitic.zamqp',
           'zope.app.testing>=3.5',
           'setuptools',
           'gites.skin',
           'gites.core',
           'affinitic.db',
-          'dateutil',
-          'simplejson',
-          'five.megrok.z3cform',
-          'p4a.calendar',
-          'p4a.plonecalendar',
-          'p4a.ploneevent',
-          'p4a.subtyper',
-          'zope.app.i18n'
       ],
-      entry_points={
-            'console_scripts':
-                ['checkCalendarActivity = gites.calendar.scripts.activity:main',
-                 'exportCalendarEvents = gites.calendar.scripts.calendar:main',
-                 'importCalendarEventsFromWalhebCalendarDaemon = gites.calendar.scripts.calendarimport:main']})
+      entry_points={})
