77e6526676eddf2157062aba00b0fb491282b19b
Laurent Lasudry <lasudry@gmail.com>
2013-02-14 17:13:18 +0100

    Add view and template for criteria values edition
    Update TODO

---
+++ gites/gdwadmin/browser/hebcriteria.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from Products.Five.browser import BrowserView
+
+
+class HebCriteriaView(BrowserView):
+    """
+    Hebs criteria values edition view
+    """
+
+    def getCriteriasValueForHeb(self, hebPk):
+        """
+        """
+        return []
