4f53fe4f6bbddc3eec728d5e96763d6572464d9d
schminitz <gillian@affinitic.be>
2013-02-14 16:13:37 +0100

    merge mapblacklist files

--- gites/gdwadmin/browser/mapblacklist.py
+++ gites/gdwadmin/browser/mapblacklist.py
@@ -40,3 +40,34 @@ class MapBlacklist(BrowserView):
                   'description': dataId + '_description',
                   'provider': dataId + '_provider'}
         return json.dumps(result)
+
+
+class MapBlacklistLiveSearch(BrowserView):
+    """
+    Map blacklist view
+    """
+
+    def __call__(self):
+        return json.dumps(['hello', 'world'])
+
+
+class MapBlacklistSearchResult(BrowserView):
+    """
+    Map blacklist search result view
+    """
+    def searchData(self):
+        searchValue = self.request.get("searchValue")
+        result = [{'id': "id1" + searchValue,
+                   'name': "item1" + searchValue,
+                   'description': "descriptionitem1",
+                   'provider': "fournisseuritem1"},
+                  {'id': "id2" + searchValue,
+                   'name': "item2" + searchValue,
+                   'description': "descriptionitem2",
+                   'provider': "fournisseuritem2"},
+                  {'id': "id3" + searchValue,
+                   'name': "item3" + searchValue,
+                   'description': "descriptionitem3",
+                   'provider': "fournisseuritem3"},
+                  ]
+        return result
