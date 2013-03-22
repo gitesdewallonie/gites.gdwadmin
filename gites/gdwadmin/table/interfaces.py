2c5cad3b012a3eacb38927ce3384d1b81572310f
Martin Peeters _martin.peeters@affinitic.be_ <martin.peeters@affinitic.be>
2013-03-22 11:00:31 +0100

    - Adds a table and a view for the hosting metadatas.

--- gites/gdwadmin/table/interfaces.py
+++ gites/gdwadmin/table/interfaces.py
@@ -4,3 +4,7 @@ from zope.interface import Interface
 class IMetadataTable(Interface):
     """
     """
+
+
+class IHebergementMetadataTable(Interface):
+    """ Marker interface for the hosting metadata table """
