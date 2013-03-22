f60d778c1293b0276f83ea03a1563e059b14ec2a
Martin Peeters _martin.peeters@affinitic.be_ <martin.peeters@affinitic.be>
2013-03-22 13:18:48 +0100

    - Adds the hosting metadata update view and table

--- gites/gdwadmin/browser/hebmetadata.py
+++ gites/gdwadmin/browser/hebmetadata.py
@@ -10,17 +10,32 @@ Copyright by Affinitic sprl
 import zope.interface
 from five import grok
 
-from gites.gdwadmin.table import hebmetadata
+from gites.gdwadmin.table import hebmetadata, hebmetadataupdate
 
 
 class HebMetadataView(grok.View):
     grok.context(zope.interface.Interface)
     grok.name(u'hebergement-metadata')
     grok.require('zope2.View')
+    grok.template('hebmetadatatable')
 
     def get_table(self):
         """ Returns the render of the table """
         table = hebmetadata.HebergementMetadataTable(self.context,
                                                      self.request)
+        table.update()
+        return table.render()
+
+
+class HebMetadataUpdateView(grok.View):
+    grok.context(zope.interface.Interface)
+    grok.name(u'hebergement-metadata-update')
+    grok.require('zope2.View')
+    grok.template('hebmetadatatable')
+
+    def get_table(self):
+        """ Returns the render of the table """
+        table = hebmetadataupdate.HebergementUpdateMetadataTable(
+            self.context, self.request)
         table.update()
         return table.render()
