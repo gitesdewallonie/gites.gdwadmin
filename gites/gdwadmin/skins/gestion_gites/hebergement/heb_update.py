30abf13cb78a9bc1f23eb317fead8907960894f1
Laurent Lasudry <lasudry@gmail.com>
2015-12-07 22:03:01 +0100

    Allow to choose epis number when no gite type is selected
    This refs #6318

--- gites/gdwadmin/skins/gestion_gites/hebergement/heb_update.py
+++ gites/gdwadmin/skins/gestion_gites/hebergement/heb_update.py
@@ -11,7 +11,7 @@
 fheb_com_fk = context.REQUEST.fheb_com_fk
 
 fheb_pk = context.REQUEST.fheb_pk
-fheb_nbre_epis = context.REQUEST.fheb_nbre_epis
+fheb_nbre_epis = context.REQUEST.get('fheb_nbre_epis')
 
 if fheb_com_fk == '0':
     return context.REQUEST.RESPONSE.redirect('standard_error_message?error_message="Code postal manquant"')
@@ -25,6 +25,7 @@ except:
     pass
 else:
     context.admin_base.hebergement.zsql_heb_app_update()
-context.hebergement.traiter_modification_lnk_heb_epis(heb_pk=fheb_pk, heb_nombre_epis=fheb_nbre_epis)
+if fheb_nbre_epis is not None:
+    context.hebergement.traiter_modification_lnk_heb_epis(heb_pk=fheb_pk, heb_nombre_epis=fheb_nbre_epis)
 
 return context.REQUEST.RESPONSE.redirect('description?fheb_pk=' + fheb_pk)
