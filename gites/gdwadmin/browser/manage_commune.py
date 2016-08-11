f1b3e3437668b6321a3055daf26464a84d343d95
Alain Meurant <alain.meurant@affinitic.be>
2016-08-11 23:53:43 +0200

    gdw-admin :: changer les noms des fichiers

--- gites/gdwadmin/browser/manage_commune.py
+++ gites/gdwadmin/browser/manage_commune.py
@@ -58,16 +58,15 @@ class CommuneView(BrowserView):
         fields = self.request.form
         wrapper = getSAWrapper('gites_wallons')
         session = wrapper.session
-        insertCommune = wrapper.getMapper('commune@')
-        newEntry = insertCommune(com_nom=fields.get('com_nom'),
-                                 com_cp=fields.get('com_cp'),
-                                 com_ins=fields.get('com_ins'),
-                                 com_id=fields.get('com_id'),
-                                 com_reg_fk=fields.get('com_reg_fk'),
-                                 com_prov_fk=fields.get('com_prov_fk'),
-                                 com_mais_fk=fields.get('com_mais_fk'))
+        newEntry = Commune(com_nom=fields.get('com_nom'),
+                           com_cp=fields.get('com_cp'),
+                           com_ins=fields.get('com_ins'),
+                           com_id=fields.get('com_id'),
+                           com_reg_fk=fields.get('com_reg_fk'),
+                           com_prov_fk=fields.get('com_prov_fk'),
+                           com_mais_fk=fields.get('com_mais_fk'))
         session.add(newEntry)
         session.flush()
-        cible = "%s/portal_skins/gestion_gites/communes/communes" % self.context.portal_url()
+        cible = "%s/portal_skins/gestion_gites/lister-les-communes" % self.context.portal_url()
         self.request.response.redirect(cible)
         return ''
